ec36b9ddb3419ceb53d94e72adccd316ddbd0e80
Won-der-dan <rominator98@gmail.com>
2018-11-28 15:08:25 -0100

    1st task fixed (a little bit), second task added, json forming added

--- grading.py
+++ grading.py
@@ -1,4 +1,6 @@
+#!/usr/bin/env python3
 import operator
+import json
 
 """
 Скрипт для расчета оценок для заданного пользователя
@@ -6,9 +8,12 @@ import operator
 
 INFILE = 'data.csv'
 OUTFILE = 'data-new.csv'
+DAYFILE = 'context_day.csv'
+PLACEFILE = 'context_place.csv'
 ID = 9
 
 
+# Считывание данных из файла
 x = {}
 userDict = dict()
 with open(INFILE) as data:
@@ -17,11 +22,27 @@ with open(INFILE) as data:
         x = line.split(', ')
         x.append(x.pop().strip())
         userDict[int(x[0].replace("User ", ""))] = x[1::]
-        #print(userDict[x[0]])
-#print(userDict)
+
 for user in userDict:
     userDict[user] = list(map(int, userDict[user]))
-    #print(user)
+#Считывание данных уже из другого файла
+x = {}
+dayDict = dict()
+with open(DAYFILE) as data:
+    data.readline()
+    for line in data:
+        x = line.split(', ')
+        x.append(x.pop().strip())
+        dayDict[int(x[0].replace("User ", ""))] = x[1::]
+#Все еще считывание данных
+x = {}
+placeDict = dict()
+with open(PLACEFILE) as data:
+    data.readline()
+    for line in data:
+        x = line.split(', ')
+        x.append(x.pop().strip())
+        placeDict[int(x[0].replace("User ", ""))] = x[1::]
 
 keyUserMovies = userDict[ID]
 moviesCount = len(keyUserMovies)
@@ -30,42 +51,81 @@ similarity = {}
 for user in userDict:
     similarMovies = list()
     userMovies = userDict[user]
-    for i in range(1, moviesCount):
-            #print(i)
-            if keyUserMovies[i] != -1 and userMovies[i] != -1:
-                similarMovies.append(i)
+
+    for i in range(0, moviesCount):
+        if keyUserMovies[i] != -1 and userMovies[i] != -1:
+            similarMovies.append(i)
+
     firstSum = 0
     secondSum = 0
     thirdSum = 0
+
     for i in similarMovies:
         firstSum += userMovies[i] * keyUserMovies[i]
         secondSum += userMovies[i]**2
         thirdSum += keyUserMovies[i]**2
+
     secondSum = secondSum ** 0.5
     thirdSum = thirdSum ** 0.5
     sim = round(firstSum / (secondSum * thirdSum), 3)
     similarity[user] = sim
-sorted_sim = sorted(similarity.items(), key=operator.itemgetter(1), reverse=True)
-sorted_sim.remove((ID, 1))
-sorted_sim = sorted_sim[0:7:]
-print(sorted_sim)
+
+del similarity[ID]
+sorted_sim = sorted(similarity.items(), key=operator.itemgetter(1),
+                    reverse=True)
+sorted_sim = sorted_sim[0:7]
 
 def avgRank(id : int):
-    list = userDict[id]
-    list = [x for x in list if x != -1]
+    list = [x for x in userDict[id] if x != -1]
     return sum(list) / float(len(list))
 
-count = 0
-for i in userDict[ID]:
-    count += 1
-    if i == -1:
+filmRecommends = {}
+for filmIndex, filmRate in enumerate(keyUserMovies):
+    if filmRate == -1:
         firstSum = 0
         secondSum = 0
-        for user in sorted_sim:
-            userRanks = userDict[user[0]]
-            firstSum += user[1]*(userRanks[count-1] - avgRank(user[0]))
-            secondSum += abs(user[1])
-        suggestedRank = round(avgRank(ID) + (firstSum/secondSum), 3)
-        keyUserMovies[count-1] = suggestedRank
+        for user, sim in sorted_sim:
+            firstSum += sim*(userDict[user][filmIndex] - avgRank(user))
+            secondSum += float(abs(sim))
 
-print(keyUserMovies)
+        suggestedRank = avgRank(ID) + (firstSum/secondSum)
+
+        # Применяем формулу для расчёта оценки фильма
+        filmRecommends[filmIndex] = round(suggestedRank, 3)
+
+
+def recommend2(dayData, placeData):
+    """
+    Рекомендует фильм на основе дополнительных данных
+    """
+    filmsToRecommend = {}
+    for filmIndex, film in enumerate(keyUserMovies):
+        if film == -1:
+            for user, sim in sorted_sim:
+                if not filmIndex in filmsToRecommend.keys():
+                    filmsToRecommend[filmIndex] = 0
+                if ( dayData[user][filmIndex] == 'Sat' or \
+                     dayData[user][filmIndex] == 'Sun' ) and \
+                    placeData[user][filmIndex] == 'h' and \
+                   sim > 0.8:
+                   filmsToRecommend[filmIndex] += float(sim)
+
+    return filmsToRecommend
+
+maxV = bestFilm = -1
+for k, v in recommend2(dayDict, placeDict).items():
+    if v > maxV:
+        maxV = v
+        bestFilm = k
+
+res2 = None
+if bestFilm > -1:
+    res2 = {
+    "movie {}".format(bestFilm): maxV
+    }
+
+print(json.dumps({
+    "user": ID,
+    1: {"movie {}".format(k):v for k,v in filmRecommends.items()},
+    2: res2
+}, indent=4))
